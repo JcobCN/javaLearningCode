75ab7eaad2e7b635a567f0aa0cbe90cacc465d88
Jiang <tj@tj.com>
2019-02-13 22:17:05 +0800

    add Enum

--- src/EnumDemo.java
+++ src/EnumDemo.java
@@ -2,10 +2,15 @@ enum Season{
     SPRING, SUMMER, AUTUMN, WINTER
 }
 
-enum HeroType{
-    private
-    HeroType(){
+enum Car{
+    lambo(900), tata(2), audi(50), fiat(15), honda(12);
+    private int price;
+    Car(int p){
+        price = p;
+    }
 
+    int getPrice(){
+        return price;
     }
 }
 
@@ -30,6 +35,9 @@ public class EnumDemo {
         for (Season s : Season.values()){
             System.out.println(s);
         }
+
+        for(Car c : Car.values())
+            System.out.println(c + "需要" + c.getPrice() + "千美元");
 
     }
 }
---
+++ src/ExtendsDemo.java
@@ -0,0 +1,18 @@
+
+class Student{
+    private String name;
+    private int age;
+
+    void study(){
+        System.out.println("studying");
+    }
+
+    void a1(){}
+
+}
+
+public class ExtendsDemo {
+    static public void main(String args[]){
+
+    }
+}
