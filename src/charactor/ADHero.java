9a6a79e3ab1508a01fa6e122617313bfe22b560f
Jiang <tj@tj.com>
2019-02-19 22:40:05 +0800

    update interface Demo

--- src/charactor/ADHero.java
+++ src/charactor/ADHero.java
@@ -1,7 +1,9 @@
 package charactor;
 
 public class ADHero extends Hero implements AD{
-    public void physicAttack(){
 
+    @Override
+    public void physicAttack(){
+        System.out.println("进行物理攻击");
     }
 }
